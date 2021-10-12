98218264a35cdd53be9d37b566b7b86da6f67a92
Srdan Rasic <srdan.rasic@gmail.com>
2021-10-13 00:10:16 +0200

    Fix an issue where binding closure could be executed after disposing

--- Sources/Bindable.swift
+++ Sources/Bindable.swift
@@ -104,11 +104,9 @@ extension SignalProtocol where Error == Never {
     /// - Returns: A disposable that can cancel the binding.
     @discardableResult
     public func bind<Target: Deallocatable>(to target: Target, context: ExecutionContext, setter: @escaping (Target, Element) -> Void) -> Disposable {
-        return prefix(untilOutputFrom: target.deallocated).observeNext { [weak target] element in
-            context.execute {
-                if let target = target {
-                    setter(target, element)
-                }
+        return prefix(untilOutputFrom: target.deallocated).receive(on: context).observeNext { [weak target] element in
+            if let target = target {
+                setter(target, element)
             }
         }
     }
@@ -188,11 +186,9 @@ extension SignalProtocol where Error == Never, Element == Void {
     /// - Returns: A disposable that can cancel the binding.
     @discardableResult
     public func bind<Target: Deallocatable>(to target: Target, context: ExecutionContext, setter: @escaping (Target) -> Void) -> Disposable {
-        return prefix(untilOutputFrom: target.deallocated).observeNext { [weak target] _ in
-            context.execute {
-                if let target = target {
-                    setter(target)
-                }
+        return prefix(untilOutputFrom: target.deallocated).receive(on: context).observeNext { [weak target] _ in
+            if let target = target {
+                setter(target)
             }
         }
     }
